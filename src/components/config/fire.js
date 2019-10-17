5ef7b5309ee549550d09e3b2b609476add5bcfc4
Ashpreet Singh <asharts6@gmail.com>
2019-10-17 14:52:40 +0530

    Initial configuration and Firebase SDK setup

---
+++ src/components/config/fire.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase"
+import "firebase/auth"
+var firebaseConfig = {
+    apiKey: "AIzaSyDgIgvc86ufxjquYsLJqgHIXvHb9NLGajU",
+    authDomain: "chat-app-28847.firebaseapp.com",
+    databaseURL: "https://chat-app-28847.firebaseio.com",
+    projectId: "chat-app-28847",
+    storageBucket: "chat-app-28847.appspot.com",
+    messagingSenderId: "1014650835908",
+    appId: "1:1014650835908:web:9399b906edddab9bd74a91"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
